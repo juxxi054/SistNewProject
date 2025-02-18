6a0f6fd9451f86d562b7c87815ccedabc52b93aa
juxxi054 <weedsrtg@naver.com>
2025-02-18 14:41:01 +0900

    k

---
+++ javac테스트/Myhobby.java
@@ -0,0 +1,12 @@
+public class Myhobby{
+
+    public static void main(String[]args){
+
+    System.out.println("내취미는"+args[0]+"입니다");
+      System.out.println("내 이름는"+args[1]+"입니다");
+       System.out.println("내 나이는"+args[2]+"입니다");
+
+
+
+  }
+}
---
+++ javaproject/q/src/q/q.java
@@ -0,0 +1,8 @@
+package q;
+
+public class q {
+	public static void main(String[] args) {
+		System.out.println("나는 주희");
+	}
+
+}
